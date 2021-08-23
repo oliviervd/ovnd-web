58478479a600541e65c98f19149bbdfc42653457
huynslol <oliviervandhuynslager@gmail.com>
2021-08-23 12:10:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>OVND</h1>
+        <p>
+          this will be the main site for my creative coding endaevours
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
