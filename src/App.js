3a816bd73f7460db3373d3ff57cee00b5f789b56
huynslol <oliviervandhuynslager@gmail.com>
2021-08-24 20:02:08 +0200

    add project page + remove redundant project files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 
 //pages
-import MainPage from "./pages";
+import MainPage from "./pages/index";
+import ProjectSum from "./pages/projects";
 
 class App extends Component {
     render() {
         return(
             <Router>
                 <Route exact path="/" component={MainPage} />
+                <Route exact path="/projects" component={ProjectSum} />
             </Router>
         );
     }
---
+++ src/pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectSum = () => {
+    return (
+        <div>
+            <h1>PROJECTS</h1>
+            <p>
+                A non-extensive list of projects exploring Creative Coding
+            </p>
+        </div>
+    )
+}
+
+export default ProjectSum;
