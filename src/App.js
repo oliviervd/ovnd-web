f7a4d0fae64aeaa934f14b8399d3de09a7eda0cf
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-10-31 17:39:19 +0100

    integrate dynamic routing to render project pages.

--- src/App.js
+++ src/App.js
@@ -10,19 +10,31 @@ import about from "./pages/about";
 import contact from "./pages/contact";
 import project from "./pages/project";
 
+//data
+import projectData from "./components/data/projects.json";
+
 class App extends Component {
+
     render() {
         return(
-            <Router>
-                <Route exact path="/" component={MainPage} self={true} />
-                <Route exact path="/projects" component={ProjectSum} self={true}/>
-                <Route exact path="/music" component={MusicMain} self={true}/>
-                <Route exact path="/about" component={about} self={true}/>
-                <Route exact path="/contact" component={contact} self={true}/>
-                <Route exact path="/music/project" component={project} self={true}/>
-            </Router>
-        );
-    }
+            <div>
+                <Router>
+                    <Route exact path="/" component={MainPage} self={true} />
+                    <Route exact path="/projects" component={ProjectSum} self={true}/>
+                    <Route exact path="/music" component={MusicMain} self={true}/>
+                    <Route exact path="/about" component={about} self={true}/>
+                    <Route exact path="/contact" component={contact} self={true}/>
+
+                    {projectData.map((postDetail, index) => {
+                        const _href = "/music/"+ postDetail.title;
+                        return(
+                            <Route exact path={_href} component={project} self={true}/>
+                        )
+                    })}
+                </Router>
+            </div>
+        )
+    };
 }
 
 export default App;
