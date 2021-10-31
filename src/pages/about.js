e07ec41fe07ce4f424e6e44193c7a0eab29523d0
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-10-31 13:12:43 +0100

    add about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Navbar from "../components/navbar/navbar"
-
-
+import { Box } from "rebass"
 
 const about = () => {
     return (
@@ -11,6 +10,19 @@ const about = () => {
         </head>
         <body>
         <Navbar/>
+        <div className="wrap">>
+            <Box flex={2}>
+                <p className="Bio">
+                    Olivier (BE) is a musician and digital creative based in Ghent, Belgium. He cultivated his love for music,
+                    design and film while studying art history and occasionally assisting on film set as a set dresser. His passion for music
+                    matured while playing in, and producing for several bands. During the day he works at the design museum on digital culture.
+                    Composing for both film and advertisement, Olivier has established himself as a freelance composer.
+                </p>
+            </Box>
+            <Box flex={1}>
+                <img className="bioPicture" src="/media/img/bio_pic.jpeg"></img>
+            </Box>
+        </div>
         </body>
         </html>
     )
