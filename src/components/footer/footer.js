d4213b909d5f41ac0d4a39ced38b7c3179313f63
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-11-07 17:08:22 +0100

    add social logos to footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,17 +3,27 @@ import { Box } from "rebass";
 
 const Footer = () => {
     return (
-        <div className={"wrap"}>
-            <Box flex={1}>
-                <p></p>
-            </Box>
-            <Box flex={2}>
-                <p><em>Legal Note. This website and its content is copyright of Olivier Van D'huynslager.
+        <div className="footer margin">
+            <div className="footer-inner-logo"e>
+                <a href="https://ovnd.bandcamp.com/"><img className="socialButton" src="media/logos/bandcamp.jpg"></img></a>
+            </div>
+            <div className="footer-inner-logo">
+                <a href="https://www.instagram.com/ovnd_music/"><img className="socialButton" src="media/logos/instagram.png"></img></a>
+            </div>
+            <div className="footer-inner-logo">
+                <a href="https://www.facebook.com/ovnd.music/"><img className="socialButton" src="media/logos/facebook.jpg"></img></a>
+            </div>
+            <div className="footer-inner-logo">
+                <a href="https://soundcloud.com/siegfreud"><img className="socialButton" src="media/logos/soundcloud.png"></img></a>
+            </div>
+            <div className="footer-inner filler"></div>
+            <div className="footer-inner">
+                <p className="margin-small-10"><em> ** Legal Note. This website and its content is copyright of Olivier Van D'huynslager.
                     Any distribution or reproduction of part or all of the contents in any form is prohibited. You may not,
                     except with our written permission, distribute or commercialy exploit the content. Nor may you transit it
                     or store it in any other website or other form of electronic retrieval system without the prior written
                     permission of Olivier Van D'huynslager.</em></p>
-            </Box>
+            </div>
         </div>
     )
 }
