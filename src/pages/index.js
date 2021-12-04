6f4f77cfdb98f8ec857676ed9a06e38a2e34b958
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-12-04 10:19:45 +0100

    remove unused feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Navbar from "../components/navbar/navbar";
 
-const navLinks = document.querySelectorAll("nav a")
-const navTextCopy = document.getElementById("nav_big_text")
-
 const MainPage = () => {
     return (
         <html>
         <head>
-            <meta httpEquiv="Content-Type" content="text/html" charset="utf-8" />
+            <meta httpEquiv="Content-Type" content="text/html" charset="utf-8"/>
             <title>home</title>
             <script type="text/javascript" src="processing.js"></script>
         </head>
@@ -25,20 +22,6 @@ const MainPage = () => {
         </body>
         </html>
     );
-}
-
-for (let i=0; i<navLinks.length; i++){
-    navLinks[i].addEventListener("mouseover",function(){
-        let navLinkText = this.textContent
-        navTextCopy.textContent = navLinkText
-        navTextCopy.classList.add("big_text_active")
-    })
-
-    navLinks[i].addEventListener("mouseout",function(){
-        let navLinkText = this.textContent
-        navTextCopy.textContent = navLinkText;
-        navTextCopy.classList.remove("big_text_active")
-    })
 }
 
 export default MainPage;
