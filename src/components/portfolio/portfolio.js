536bbac7e9fd85a2145a271ec470ab9196019b61
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-12-04 18:12:24 +0100

    add media query

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,9 +1,21 @@
 import React from "react";
 import projectData from "../data/projectsMusic.json";
 import { Box } from "rebass"
+import { useMediaQuery } from "react-responsive";
+import Footer from "../footer/footer";
+
 
 const Portfolio = () => {
 
+    //media-query
+    const Desktop = ({ children }) => {
+        const isDesktop = useMediaQuery({ minWidth: 786 })
+        return isDesktop? children: null
+    }
+    const Mobile = ({ children }) => {
+        const isMobile = useMediaQuery({ maxWidth: 786 })
+        return isMobile? children: null
+    }
 
     return (
         <div>
@@ -12,19 +24,36 @@ const Portfolio = () => {
                 //fetch img from db and construct path.
                 const _href = "/music/"+ postDetail.id;
                 return (
-                    <div className={"wrap"}>
-                        <div className="portfolioLine">
-                            <Box flex={0.5}>
-                                <h5 className="subText">{postDetail.year}</h5>
-                            </Box>
-                            <Box flex={1}>
-                                <h3><a href={_href} target="_self">{postDetail.title}</a></h3>
-                            </Box>
-                            <Box flex={2} className="gridInfo">
-                                <h5 className="subText">{postDetail.type}</h5>
-                            </Box>
-                        </div>
-                    </div>
+                    <container>
+                        <Desktop>
+                            <div className={"wrap"}>
+                                <div className="portfolioLine">
+                                    <Box flex={0.5}>
+                                        <h5 className="subText">{postDetail.year}</h5>
+                                    </Box>
+                                    <Box flex={1}>
+                                        <h3><a href={_href} target="_self">{postDetail.title}</a></h3>
+                                    </Box>
+                                    <Box flex={1}>
+                                        <h5 className="subText">{postDetail.type}</h5>
+                                    </Box>
+                                </div>
+                            </div>
+                        </Desktop>
+
+                        <Mobile>
+                            <div className={"wrap"}>
+                                <div className="portfolioLine">
+                                    <Box flex={0.5}>
+                                        <h5 className="subText">{postDetail.year}</h5>
+                                    </Box>
+                                    <Box flex={1}>
+                                        <h3><a href={_href} target="_self">{postDetail.title}</a></h3>
+                                    </Box>
+                                </div>
+                            </div>
+                        </Mobile>
+                    </container>
                 )
             })}
         </div>
