a3d8694356946639af7e677024608043c21a093a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-12-05 13:33:11 +0100

    fix navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -22,9 +22,9 @@ const Navbar = () => {
         setNavbarOpen(prev => !prev)
     }
     return(
-        <div className={'sticky'}>
-            <nav>
-                <Desktop>
+        <div>
+            <Desktop>
+                    <nav>
                     <container>
                         <div className="nav_menu">
                             <ul>
@@ -36,27 +36,30 @@ const Navbar = () => {
                             </ul>
                         </div>
                     </container>
-                </Desktop>
+                    </nav>
+            </Desktop>
 
                 <Mobile>
-                    <container>
-                        <nav className="navBurger">
-                            <button onClick={handleToggle}>{navbarOpen ? (
-                                <MdClose style={{color: "#FFCDD2", width: "40px", height: "40px"}}/>
-                            ) : (
-                                <VscMenu style={{color: "#FFCDD2", width: "40px", height: "40px"}}/>
-                            )}
-                            </button>
-                            <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/" target="_self">home</a></li>
-                            <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/projects" target="_self">c_coding</a></li>
-                            <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/music" target="_self">music</a></li>
-                            <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/about" target="_self">about</a></li>
-                            <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/contact" target="_self">contact</a></li>
+                    <div className={"sticky"}>
+                        <nav>
+                        <container>
+                            <nav className="navBurger">
+                                <button onClick={handleToggle}>{navbarOpen ? (
+                                    <MdClose style={{color: "#FFCDD2", width: "40px", height: "40px"}}/>
+                                ) : (
+                                    <VscMenu style={{color: "#FFCDD2", width: "40px", height: "40px"}}/>
+                                )}
+                                </button>
+                                <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/" target="_self">home</a></li>
+                                <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/projects" target="_self">c_coding</a></li>
+                                <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/music" target="_self">music</a></li>
+                                <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/about" target="_self">about</a></li>
+                                <li className={`menuNavBurger${navbarOpen ? " showMenu": ""}`}><a href="/contact" target="_self">contact</a></li>
+                            </nav>
+                        </container>
                         </nav>
-                    </container>
+                    </div>
                 </Mobile>
-
-            </nav>
         </div>
     )
 }
