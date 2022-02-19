037c39178222362967fa337cc40fc5eccd9ae2e5
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-02-19 17:38:16 +0100

    clean up code

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import ProjectSum from "./pages/projects";
 import MusicMain from "./pages/musicMain";
 import about from "./pages/about";
 import project from "./pages/project";
+import bruises from "./projects/bruises"
 
 class App extends Component {
 
     render() {
         return(
-            <div>
+
                 <Router>
                     <Route exact path="/" component={MainPage} self={true} />
                     <Route exact path="/projects" component={ProjectSum} self={true}/>
+                    <Route exact path="/projects/bruises" component={bruises} self={true}/>
                     <Route exact path="/music" component={MusicMain} self={true}/>
                     <Route exact path="/about" component={about} self={true}/>
                     <Route exact path="/music/:id" component={project} self={true}/>
                 </Router>
-            </div>
         )
     };
 }
